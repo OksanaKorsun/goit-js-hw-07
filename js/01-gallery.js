7af824f271fa78d5588b5071dffc790575df83f6
OksanaKorsun <oksana2081989@gmail.com>
2023-08-10 19:40:58 +0300

    homework-07-commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+// console.log(basicLightbox);
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+gallery.addEventListener("click", handlerClick);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function handlerClick(evt) {
+  evt.preventDefault();
+  // if (!event.target.classList.contains('gallery__image'))
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  // const currentImg = evt.target.closest(".gallery__item");
+  // const source = currentImg.dataset.source;
+  // console.log(source);
+  // const image = galleryItems.find(
+  //   ({ original }) => original === source
+  // );
+  const instance = basicLightbox.create(
+    `<div class="modal"><img width="1080" src="${evt.target.dataset.source}"/>
+</div>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEsc);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEsc);
+      },
+    }
+  );
+  instance.show();
+  function onEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
